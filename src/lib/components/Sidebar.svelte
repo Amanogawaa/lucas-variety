47f71db284469edc6341e85e47f262ded5787841
amu <dommolino28@gmail.com>
2025-02-01 18:43:30 +0800

    setup proj

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Computer, Contact, InfoIcon, LayoutDashboard, Package, type Icon } from 'lucide-svelte';
+
+	type MenuTypes = {
+		name: string;
+		href: string;
+		icon: typeof Icon;
+	};
+
+	const menu: MenuTypes[] = [
+		{
+			name: 'Dashboard',
+			href: '/dashboard',
+			icon: LayoutDashboard
+		},
+
+		{
+			name: 'Point of Sales',
+			href: '/pos',
+			icon: Computer
+		},
+
+		{
+			name: 'Inventory',
+			href: '/inventory',
+			icon: Package
+		}
+	];
+</script>
+
+<aside class="fixed bottom-1 left-1 top-1 flex w-12 flex-col rounded-md bg-gray-800 text-white">
+	<div>
+		<header>Header Icon here</header>
+	</div>
+	<div class="flex flex-grow flex-col items-center gap-2 p-2">
+		{#each menu as item, i}
+			{@const Icon = item.icon}
+			<div>
+				<a href={item.href}>
+					<Icon class="h-5 w-5" />
+					<span class="sr-only">{item.name}</span>
+				</a>
+			</div>
+		{/each}
+	</div>
+	<div>
+		<footer>Footer heere</footer>
+	</div>
+</aside>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+
+	// define routes here
+
+	let { children } = $props();
+</script>
+
+<main>
+	<Sidebar />
+	{@render children()}
+</main>
